ea08698ce95d0a56ca953e3a317959df70884206
Saransh Bari <131943503+SaranshPrasad@users.noreply.github.com>
2024-12-30 12:31:50 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -42,10 +42,7 @@ const Login = () => {
       const response = await axios.post(`${database_url}/auth/login`, {
         email,
         password,
-      },{withCredentials: true, // Required to handle cookies
-    headers: {
-        "Content-Type": "application/json",
-    }});
+      });
 
       if (response.status === 200) {
         const token = response.data.token;
