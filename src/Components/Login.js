32a9ebd7b697eb31547ab5e3f3a9d56b1d341641
Saransh Bari <131943503+SaranshPrasad@users.noreply.github.com>
2024-12-30 12:06:43 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -42,7 +42,7 @@ const Login = () => {
       const response = await axios.post(`${database_url}/auth/login`, {
         email,
         password,
-      },{withCredentials: true,credentials: "include", // Required to handle cookies
+      },{withCredentials: true, // Required to handle cookies
     headers: {
         "Content-Type": "application/json",
     }});
